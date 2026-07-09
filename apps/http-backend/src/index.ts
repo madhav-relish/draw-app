342c6d0d76a79d914993d7455d10953922d75659
Madhav <madhavsingh.relish@gmail.com>
2026-07-09 21:50:22 +0530

    Added prisma and the signup route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,22 +3,41 @@ import jwt from 'jsonwebtoken';
 import { middleware } from './middleware';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { CreateRoomSchema, CreateUserSchema, SignInSchema } from '@repo/common/types';
+import { prismaClient } from "@repo/db/client"
 
 const app = express();
 
-app.post('/singup', (req, res) => {
+app.use(express.json())
+
+app.post('/singup', async(req, res) => {
     //db call
 
-    const data = CreateUserSchema.safeParse(req.body)
-    if (!data.success) {
+    const parsedData = CreateUserSchema.safeParse(req.body)
+    if (!parsedData.success) {
         return res.json({
             message: "Incorrect credentials"
         })
     }
 
-    res.json({
-        userId: 123
-    })
+    try {
+    const user =  await prismaClient.user.create({
+            data: {
+                email: parsedData.data.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+
+        res.json({
+            userId: user.id
+        })
+    } catch (error) {
+        res.status(401).json({
+           message: "Something is wrong with the data"
+        })
+    }
+
+
 })
 
 
@@ -44,12 +63,19 @@ app.post('/singin', (req, res) => {
 app.post('/create-room', middleware, (req, res) => {
     //db call
 
-    const data = CreateRoomSchema.safeParse(req.body)
-    if (!data.success) {
+    const parsedData = CreateRoomSchema.safeParse(req.body)
+    if (!parsedData.success) {
         return res.json({
             message: "Incorrect credentials"
         })
     }
+
+    // prismaClient.room.create({
+    //     data: {
+    //         slug: parsedData?.data?.name,
+
+    //     }
+    // })
     res.json({
         roomId: 123
     })
